978ea752b512a8d1a59755e6b1f486451afa3a93
Kevin Smith <smith.ks64@gmail.com>
2014-03-23 15:51:15 -0400

    add bookmark class

--- bookmark.rb
+++ bookmark.rb
@@ -1,1 +1,14 @@
-Test
+class Bookmark
+  attr_accessor :url, :title
+  attr_reader :last_visted
+
+  def initialize(url, title)
+    @url = url
+    @title = title
+    @last_visited = Time.now
+  end
+
+  def visit!
+    @last_visited = Time.now
+  end
+end
